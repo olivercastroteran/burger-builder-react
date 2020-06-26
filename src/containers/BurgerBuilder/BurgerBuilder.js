e166c88cff45ebc794f6d0ed59609999071ec015
olivercastroteran <olivercastroteran@gmail.com>
2020-06-26 11:14:26 -0400

    cancel and continue btns added to OrderSummary component

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -82,6 +82,10 @@ class BurgerBuilder extends Component {
     this.setState({ purchasing: false });
   };
 
+  purchaseContinueHandler = () => {
+    alert('Success!!!');
+  };
+
   render() {
     const disabledInfo = {
       ...this.state.ingredients,
@@ -98,7 +102,11 @@ class BurgerBuilder extends Component {
           show={this.state.purchasing}
           modalClosed={this.purchaseCancelHandler}
         >
-          <OrderSummary ingredients={this.state.ingredients} />
+          <OrderSummary
+            ingredients={this.state.ingredients}
+            purchaseCancelled={this.purchaseCancelHandler}
+            purchaseContinued={this.purchaseContinueHandler}
+          />
         </Modal>
         <Burger ingredients={this.state.ingredients} />
         <BuildControls
