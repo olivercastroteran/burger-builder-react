b796f8f20458fbeaf0e2fb67a1194a9011b64081
olivercastroteran <olivercastroteran@gmail.com>
2020-06-26 22:39:22 -0400

    SideDrawer component added

--- src/components/Navigation/SideDrawer/SideDrawer.js
+++ src/components/Navigation/SideDrawer/SideDrawer.js
@@ -1,20 +1,20 @@
 import React from 'react';
-import classes from './Toolbar.module.css';
 import Logo from '../../Logo/Logo';
 import NavigationItems from '../NavigationItems/NavigationItems';
+import classes from './SideDrawer.module.css';
 
-const Toolbar = (props) => {
+const SideDrawer = (props) => {
+  // ...
   return (
-    <header className={classes.Toolbar}>
-      <div>MENU</div>
+    <div className={classes.SideDrawer}>
       <div className={classes.Logo}>
         <Logo />
       </div>
       <nav>
         <NavigationItems />
       </nav>
-    </header>
+    </div>
   );
 };
 
-export default Toolbar;
+export default SideDrawer;
