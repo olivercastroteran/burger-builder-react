4d63cba534877e9ec2b5aeb7203dc6f9878cfed3
olivercastroteran <olivercastroteran@gmail.com>
2020-06-30 20:43:12 -0400

    connecting the app to firebase realtime database

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -4,6 +4,7 @@ import Burger from '../../components/Burger/Burger';
 import BuildControls from '../../components/Burger/BuildControls/BuildControls';
 import Modal from '../../components/UI/Modal/Modal';
 import OrderSummary from '../../components/Burger/OrderSummary/OrderSummary';
+import axios from '../../axios-orders';
 
 const INGREDIENT_PRICES = {
   salad: 0.5,
@@ -83,7 +84,26 @@ class BurgerBuilder extends Component {
   };
 
   purchaseContinueHandler = () => {
-    alert('Success!!!');
+    //alert('Success!!!');
+    const order = {
+      ingredients: this.state.ingredients,
+      price: this.state.totalPrice.toFixed(2),
+      customer: {
+        name: 'Oliver Castro',
+        address: {
+          street: 'TestStreet 1',
+          zipcode: '56987',
+          country: 'Bolivia',
+        },
+        email: 'test@test.com',
+      },
+      deliveryMethod: 'fastest',
+    };
+
+    axios
+      .post('/orders.json', order)
+      .then((res) => console.log(res))
+      .catch((error) => console.log(error));
   };
 
   render() {
