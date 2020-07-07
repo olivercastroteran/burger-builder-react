62c408da0513f46a7316c0820723e6115c26b8f9
olivercastroteran <olivercastroteran@gmail.com>
2020-07-06 22:04:56 -0400

    implemented routing on checkout components

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -25,6 +25,7 @@ class BurgerBuilder extends Component {
   };
 
   componentDidMount() {
+    console.log(this.props);
     axios
       .get('https://react-my-burger-9ddaf.firebaseio.com/ingredients.json')
       .then((res) => {
@@ -95,31 +96,30 @@ class BurgerBuilder extends Component {
 
   purchaseContinueHandler = () => {
     //alert('Success!!!');
-    this.setState({ loading: true });
-
-    const order = {
-      ingredients: this.state.ingredients,
-      price: this.state.totalPrice.toFixed(2),
-      customer: {
-        name: 'Oliver Castro',
-        address: {
-          street: 'TestStreet 1',
-          zipcode: '56987',
-          country: 'Bolivia',
-        },
-        email: 'test@test.com',
-      },
-      deliveryMethod: 'fastest',
-    };
-
-    axios
-      .post('/orders.json', order)
-      .then((res) => {
-        this.setState({ loading: false, purchasing: false });
-      })
-      .catch((error) => {
-        this.setState({ loading: false, purchasing: false });
-      });
+    // this.setState({ loading: true });
+    // const order = {
+    //   ingredients: this.state.ingredients,
+    //   price: this.state.totalPrice.toFixed(2),
+    //   customer: {
+    //     name: 'Oliver Castro',
+    //     address: {
+    //       street: 'TestStreet 1',
+    //       zipcode: '56987',
+    //       country: 'Bolivia',
+    //     },
+    //     email: 'test@test.com',
+    //   },
+    //   deliveryMethod: 'fastest',
+    // };
+    // axios
+    //   .post('/orders.json', order)
+    //   .then((res) => {
+    //     this.setState({ loading: false, purchasing: false });
+    //   })
+    //   .catch((error) => {
+    //     this.setState({ loading: false, purchasing: false });
+    //   });
+    this.props.history.push('/checkout');
   };
 
   render() {
