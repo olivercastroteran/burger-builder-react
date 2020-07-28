b4a62c78688409eb22692703aff7b26a36890a9a
olivercastroteran <olivercastroteran@gmail.com>
2020-07-28 16:29:40 -0400

    logout func after 1hr implemented

--- src/store/actions/auth.js
+++ src/store/actions/auth.js
@@ -22,6 +22,20 @@ export const authFail = (error) => {
   };
 };
 
+export const logout = () => {
+  return {
+    type: actionTypes.AUTH_LOGOUT,
+  };
+};
+
+export const checkAuthTimeout = (expirationTime) => {
+  return (dispatch) => {
+    setTimeout(() => {
+      dispatch(logout());
+    }, expirationTime * 1000);
+  };
+};
+
 export const auth = (email, password, isSignup) => {
   return (dispatch) => {
     dispatch(authStart());
@@ -40,9 +54,10 @@ export const auth = (email, password, isSignup) => {
 
     axios
       .post(url, authData)
-      .then((res) => {
-        console.log(res);
-        dispatch(authSuccess(res.data.idToken, res.data.localId));
+      .then((response) => {
+        console.log(response);
+        dispatch(authSuccess(response.data.idToken, response.data.localId));
+        dispatch(checkAuthTimeout(response.data.expiresIn));
       })
       .catch((err) => {
         console.log(err);
