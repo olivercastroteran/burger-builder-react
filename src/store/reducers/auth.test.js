e23c1cd0063f1104a88779260c5cfd92fd57363c
olivercastroteran <olivercastroteran@gmail.com>
2020-07-30 18:09:17 -0400

    auth reducer test added

---
+++ src/store/reducers/auth.test.js
@@ -0,0 +1,39 @@
+import authReducer from './auth';
+import * as actionTypes from '../actions/actionTypes';
+
+describe('auth reducer', () => {
+  it('should return the initial state', () => {
+    expect(authReducer(undefined, {})).toEqual({
+      token: null,
+      userId: null,
+      error: null,
+      loading: false,
+      authRedirectPath: '/',
+    });
+  });
+
+  it('should store the token upon login', () => {
+    expect(
+      authReducer(
+        {
+          token: null,
+          userId: null,
+          error: null,
+          loading: false,
+          authRedirectPath: '/',
+        },
+        {
+          type: actionTypes.AUTH_SUCCESS,
+          idToken: 'some-token',
+          userId: 'some-userId',
+        }
+      )
+    ).toEqual({
+      token: 'some-token',
+      userId: 'some-userId',
+      error: null,
+      loading: false,
+      authRedirectPath: '/',
+    });
+  });
+});
