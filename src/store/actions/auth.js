1cf98c18e5e1e846085466059447887228907208
olivercastroteran <olivercastroteran@gmail.com>
2020-08-15 21:46:07 -0400

    handling authentication with a saga

--- src/store/actions/auth.js
+++ src/store/actions/auth.js
@@ -1,5 +1,5 @@
 import * as actionTypes from './actionTypes';
-import axios from 'axios';
+//import axios from 'axios';
 
 export const authStart = () => {
   return {
@@ -50,38 +50,44 @@ export const checkAuthTimeout = (expirationTime) => {
 };
 
 export const auth = (email, password, isSignup) => {
-  return (dispatch) => {
-    dispatch(authStart());
-    const API_KEY = 'AIzaSyCKwzL9M5umQ6oP342UX9t09c-k4ZtHnY4';
-    const authData = {
-      email: email,
-      password: password,
-      returnSecureToken: true,
-    };
+  // return (dispatch) => {
+  //   dispatch(authStart());
+  //   const API_KEY = 'AIzaSyCKwzL9M5umQ6oP342UX9t09c-k4ZtHnY4';
+  //   const authData = {
+  //     email: email,
+  //     password: password,
+  //     returnSecureToken: true,
+  //   };
 
-    let url = `https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=${API_KEY}`;
+  //   let url = `https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=${API_KEY}`;
 
-    if (!isSignup) {
-      url = `https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=${API_KEY}`;
-    }
+  //   if (!isSignup) {
+  //     url = `https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=${API_KEY}`;
+  //   }
 
-    axios
-      .post(url, authData)
-      .then((response) => {
-        const expirationDate = new Date(
-          new Date().getTime() + response.data.expiresIn * 1000
-        );
+  //   axios
+  //     .post(url, authData)
+  //     .then((response) => {
+  //       const expirationDate = new Date(
+  //         new Date().getTime() + response.data.expiresIn * 1000
+  //       );
 
-        localStorage.setItem('token', response.data.idToken);
-        localStorage.setItem('expirationDate', expirationDate);
-        localStorage.setItem('userId', response.data.localId);
+  //       localStorage.setItem('token', response.data.idToken);
+  //       localStorage.setItem('expirationDate', expirationDate);
+  //       localStorage.setItem('userId', response.data.localId);
 
-        dispatch(authSuccess(response.data.idToken, response.data.localId));
-        dispatch(checkAuthTimeout(response.data.expiresIn));
-      })
-      .catch((err) => {
-        dispatch(authFail(err.response.data.error));
-      });
+  //       dispatch(authSuccess(response.data.idToken, response.data.localId));
+  //       dispatch(checkAuthTimeout(response.data.expiresIn));
+  //     })
+  //     .catch((err) => {
+  //       dispatch(authFail(err.response.data.error));
+  //     });
+  // };
+  return {
+    type: actionTypes.AUTH_USER,
+    email: email,
+    password: password,
+    isSignup: isSignup,
   };
 };
 
--- src/store/sagas/auth.js
+++ src/store/sagas/auth.js
@@ -1,5 +1,6 @@
 import { put, delay } from 'redux-saga/effects';
 import * as actions from '../actions/index';
+import axios from 'axios';
 
 export function* logoutSaga(action) {
   yield localStorage.removeItem('token');
@@ -11,4 +12,39 @@ export function* logoutSaga(action) {
 export function* checkAuthTimeoutSaga(action) {
   yield delay(action.expirationTime * 1000);
   yield put(actions.logout());
+}
+
+export function* authUserSaga(action) {
+  yield put(actions.authStart());
+  const API_KEY = 'AIzaSyCKwzL9M5umQ6oP342UX9t09c-k4ZtHnY4';
+  const authData = {
+    email: action.email,
+    password: action.password,
+    returnSecureToken: true,
+  };
+
+  let url = `https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=${API_KEY}`;
+
+  if (!action.isSignup) {
+    url = `https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=${API_KEY}`;
+  }
+
+  try {
+    const response = yield axios.post(url, authData);
+
+    const expirationDate = yield new Date(
+      new Date().getTime() + response.data.expiresIn * 1000
+    );
+
+    yield localStorage.setItem('token', response.data.idToken);
+    yield localStorage.setItem('expirationDate', expirationDate);
+    yield localStorage.setItem('userId', response.data.localId);
+
+    yield put(
+      actions.authSuccess(response.data.idToken, response.data.localId)
+    );
+    yield put(actions.checkAuthTimeout(response.data.expiresIn));
+  } catch (error) {
+    yield put(actions.authFail(error.response.data.error));
+  }
 }
